f584dd6745dd93d9ee0a68a2f9b8d58d185f1c2d
evansekeful <ellie@solnimbus.com>
2017-04-30 19:34:25 -0400

    marked up justified album behavior for reference

--- album-foo-justified-album.php
+++ album-foo-justified-album.php
@@ -28,24 +28,23 @@ $caption_source = foogallery_album_template_setting( 'caption_source', 'title' )
 
 <div data-justified-options='{ "rowHeight": <?php echo $row_height; ?>, "maxRowHeight": <?php echo $max_row_height; ?>, "margins": <?php echo $margins; ?>, "captions": <?php echo $captions ? 'true' : 'false'; ?> }' id="foogallery-gallery-album-<?php echo $current_foogallery_album->ID; ?>" class="<?php foogallery_build_class_attribute_render_safe( $current_foogallery_album, 'foogallery-lightbox-' . $lightbox, 'foogallery-justified-loading' ); ?>">
 	<?php foreach ( $current_foogallery_album->galleries() as $gallery ) {
-
+		// Check for Featured image
 		if (!empty($gallery->attachment_ids)) {
 		$attachment = $gallery->featured_attachment();
-
 		if ( false === $attachment ) continue;
-
+		// Save cover image into variable
 		$img_html = $attachment->html_img( $args );
-
+		// Build gallery links
 		$gallery_link = foogallery_album_build_gallery_link( $current_foogallery_album, $gallery );
 		$gallery_link_target = foogallery_album_build_gallery_link_target( $current_foogallery_album, $gallery );
-
+		// Build gallery title
 		if ( 'title' == $caption_source ) {
 			$gallery->alt = $gallery->title;
 		} else if ( 'caption' == $caption_source ) {
 			$gallery->alt = $gallery->caption;
 		}
+		// Display Gallery
 
-		echo $attachment->html_img( $args );
 	}
 } ?>
 </div>
